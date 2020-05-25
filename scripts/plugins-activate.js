51cb1841a7b8c37ac83c54a5cd4353c3c3e2974e
Iban Dasca <iban.dasca@xing.com>
2020-05-25 20:23:51 +0200

    Initial commit

---
+++ scripts/plugins-activate.js
@@ -0,0 +1,29 @@
+(function ($jq) {
+  $jq(document).ready(function () {
+    //  TESTIMONIALS CAROUSEL HOOK
+  //   var owl = $jq("#customers-testimonials");
+  //   owl.owlCarousel({
+  //     loop: true,
+  //     center: true,
+  //     items: 3,
+  //     margin: 0,
+  //     autoplay: true,
+  //     dots: true,
+  //     autoplayTimeout: 8500,
+  //     smartSpeed: 450,
+  //     responsive: {
+  //       0: {
+  //         items: 1,
+  //       },
+  //       768: {
+  //         items: 2,
+  //       },
+  //       1170: {
+  //         items: 3,
+  //       },
+  //     },
+  //     navigation: true,
+  //     navigationText: true,
+  //   });
+  // });
+})(jQuery);
